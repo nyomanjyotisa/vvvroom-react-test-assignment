85996f01176b0e4900ab67ac86a91f325f2c085d
I Nyoman Jyotisa <jyotisa1616@gmail.com>
2024-02-10 14:21:06 +0800

    Added Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold text-red-500 underline text-center">Hello world!</h1> 
-  )
+    return (
+        <h1 className="text-3xl font-bold text-red-500 underline text-center">
+            Hello world!
+        </h1>
+    )
 }
 
 export default App
